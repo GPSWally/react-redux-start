17dbc6404515aec3a2d7377d64038682a7bd0595
Brian Walter <brian.walter@gmail.com>
2017-11-08 09:59:55 -0700

    added files from course video

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 /* eslint-disable no-console */
-console.log('hi');
+//console.log('hi');
+
+import 'babel-polyfill';  //fills in issue with babel compiled
+import React from 'react';
+import { render } from 'react-dom';  // needed for render function
+import { Router, browserHistory } from 'react-router';  //how to handle history
+import routes from './routes'; //referencing the routes file, which is used below.
+import './styles/styles.css'; //Webpack can import CSS file too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';  //where bootstrap is installed.
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
